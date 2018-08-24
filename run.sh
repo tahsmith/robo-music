d57805ae875bdc99a6b03e4699dd40ce93cdd248
tahsmith <tahs88@gmail.com>
2018-08-24 22:05:30 +1000

    put manual env back in

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
 pyenv install -s 3.6.4
 pyenv local 3.6.4
 python -m venv venv
