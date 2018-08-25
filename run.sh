b1afff0d5cf7c141bb7acf8dfebef3c734e26713
tahsmith <tahs88@gmail.com>
2018-08-25 11:53:18 +1000

    clean up run script

--- run.sh
+++ run.sh
@@ -5,22 +5,7 @@ set -e
 rm -rf cache
 rm -rf logs
 
-echo "$BASH_ENV"
-
-exit 1
-
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
-
-pyenv install -s 3.6.4
-pyenv local 3.6.4
-python -m venv venv
-source venv/bin/activate
-
-pip install -r requirements.txt >/dev/null
+source ./env.sh
 
 py.test --junitxml=result.xml test
 
